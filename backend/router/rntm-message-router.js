57d4542721941ce6e4dee3843221ccce701131dc
ARSENPC\matev\Desktop <matevosyan2000@gmail.com>
2021-01-23 21:56:49 +0400

    ADD mail to user

--- backend/router/rntm-message-router.js
+++ backend/router/rntm-message-router.js
@@ -1,11 +1,12 @@
 const controller = require("../controller/rntm-meesage-controller");
-const mail = require("../mail_controller/rntm-mail");
+const {sendMailToUser, sendMailRntm} = require("../mail_controller/rntm-mail");
 var validator = require("email-validator");
 
 function saveUserMessage(req, res) {
     console.log(req.body);
     if (validator.validate(req.body.email)) {
-        mail.sendMailRntm(req);
+        sendMailRntm(req);
+        sendMailToUser(req, res);
         controller.saveMessage(req.body);
     }
 }
