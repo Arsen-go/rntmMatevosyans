8eeebd6f7ab52a0e16ff1378b1ab16420d355b87
ARSENPC\matev\Desktop <matevosyan2000@gmail.com>
2021-01-30 00:48:21 +0400

    HOT FIX

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
 });
 
 app.get("/", (req,res) => { 
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname,"..","/html/index.html"));
 
  });
 
